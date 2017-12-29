51d18da0a5722f7b043959e083b7568b6d1e72ae
Milosz Dura <milosz.md@wp.pl>
2017-12-29 19:13:32 +0000

    adding ability to see current location coordinates

--- DealFinder.Web/src/app/app.component.ts
+++ DealFinder.Web/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    @Input() feedback;
+
+    findLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                console.log(position);
+                this.feedback = `lat: ${position.coords.latitude} lng: ${position.coords.longitude}`;
+            });
+            return;
+        }
+
+        this.feedback = 'Geolocation is not supported by this browser.';
+    }
 }
--- DealFinder.Web/src/environments/environment.prod.ts
+++ DealFinder.Web/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: 'https://fyp.miloszdura.com/api/'
 };
