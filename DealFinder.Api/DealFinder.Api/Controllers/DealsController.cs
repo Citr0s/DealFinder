20e2424f39cdc86a89c20419383f1710f95b8452
Milosz Dura <milosz.md@wp.pl>
2017-12-30 21:18:00 +0000

    adding new endpoint to return data supplied by front end, added test project

---
+++ DealFinder.Api/DealFinder.Api/Controllers/DealsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DealFinder.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class DealsController : Controller
+    {
+        [HttpGet("{latitude}/{longitude}")]
+        public ActionResult Get(double latitude, double longitude)
+        {
+            return Ok(new
+            {
+                Latitude = latitude,
+                Longitude = longitude
+            });
+        }
+    }
+}
--- DealFinder.Web/src/app/app.component.ts
+++ DealFinder.Web/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import {environment} from '../environments/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -7,12 +9,22 @@ import {Component, Input} from '@angular/core';
 })
 export class AppComponent {
     @Input() feedback;
+    private _httpClient: HttpClient;
+
+    constructor(httpClient: HttpClient) {
+        this._httpClient = httpClient;
+
+    }
 
     findLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
-                console.log(position);
-                this.feedback = `lat: ${position.coords.latitude} lng: ${position.coords.longitude}`;
+                this._httpClient
+                    .get(`${environment.backendUrl}deals/${position.coords.latitude}/${position.coords.longitude}`)
+                    .subscribe((payload) => {
+                        console.log(payload);
+                        this.feedback = payload;
+                    });
             });
             return;
         }
