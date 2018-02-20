85d0b7b9b505170c993defd554d05398d26863aa
Milosz Dura <miloszdura@gmail.com>
2018-02-20 17:30:21 +0000

    minor styling fixes on mobile devices.
    setting up new page for creating new deal entries.

---
+++ DealFinder.Web/src/pages/new-deal-page/new-deal-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DealsModel } from './deals.model';
+
+@Component({
+    selector: 'new-deal-page',
+    templateUrl: './new-deal-page.component.html',
+    styleUrls: ['./new-deal-page.component.scss']
+})
+export class HomePageComponent {
+
+    constructor() {
+    }
+}
