f418a8bd836e6b2d1aec3e84c677b20c6fc76da6
Milosz Dura <milosz.md@wp.pl>
2018-02-21 19:24:37 +0000

    adding loading spinner when nearby deals are loading

--- DealFinder.Web/src/app/app.module.ts
+++ DealFinder.Web/src/app/app.module.ts
@@ -1,39 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { appRoutes } from './app.routes';
-import { RouterModule } from '@angular/router';
-import { HomePageComponent } from "../pages/home-page/home-page.component";
-import { DealsService } from "../shared/deals/deals.service";
-import { DealsRepository } from "../shared/deals/deals.repository";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {appRoutes} from './app.routes';
+import {RouterModule} from '@angular/router';
+import {HomePageComponent} from '../pages/home-page/home-page.component';
+import {DealsService} from '../shared/deals/deals.service';
+import {DealsRepository} from '../shared/deals/deals.repository';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
-    MatButtonModule, MatCardModule, MatDividerModule, MatFormFieldModule, MatIconModule, MatInputModule,
+    MatButtonModule, MatCardModule, MatDividerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule
-} from "@angular/material";
-import { NewDealPageComponent } from "../pages/new-deal-page/new-deal-page.component";
-import { SignUpPageComponent } from "../pages/sign-up-page/sign-up-page.component";
+} from '@angular/material';
+import {NewDealPageComponent} from '../pages/new-deal-page/new-deal-page.component';
+import {SignUpPageComponent} from '../pages/sign-up-page/sign-up-page.component';
 import {
     SocialLoginModule,
     AuthServiceConfig,
     GoogleLoginProvider,
     FacebookLoginProvider,
-} from "angular5-social-login";
-import { UserService } from "../shared/user/user.service";
-import { UserRepository } from "../shared/user/user.repository";
+} from 'angular5-social-login';
+import {UserService} from '../shared/user/user.service';
+import {UserRepository} from '../shared/user/user.repository';
 
 export function getAuthServiceConfigs() {
     let config = new AuthServiceConfig(
         [
             {
                 id: FacebookLoginProvider.PROVIDER_ID,
-                provider: new FacebookLoginProvider("Your-Facebook-app-id")
+                provider: new FacebookLoginProvider('Your-Facebook-app-id')
             },
             {
                 id: GoogleLoginProvider.PROVIDER_ID,
-                provider: new GoogleLoginProvider("335909346629-qaf57344qo332figcf662t3c7hgndtk2")
+                provider: new GoogleLoginProvider('335909346629-qaf57344qo332figcf662t3c7hgndtk2')
             },
         ]
     );
@@ -60,6 +60,7 @@ export function getAuthServiceConfigs() {
         MatDividerModule,
         MatInputModule,
         MatFormFieldModule,
+        MatProgressSpinnerModule,
         SocialLoginModule
     ],
     providers: [
