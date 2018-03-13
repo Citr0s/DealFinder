886f596765eaa17e17927e8f7eaf14410b0514e7
Milosz Dura <milosz.md@wp.pl>
2018-03-13 14:33:36 +0000

    highlighting deploy script messages

--- deploy.sh
+++ deploy.sh
@@ -3,26 +3,26 @@
 GREEN='\033[0;32m' # Green
 NC='\033[0m' # No Color
 
-echo '${GREEN}*** Killing dotnet process... ***${NC}';
+echo -e '${GREEN}*** Killing dotnet process... ***${NC}';
 pkill -HUP dotnet
 
-echo '${GREEN}*** Getting latest changes from git... ***${NC}';
+echo -e '${GREEN}*** Getting latest changes from git... ***${NC}';
 git pull
 
-echo '${GREEN}*** Changing directory to Web ***${NC}';
+echo -e '${GREEN}*** Changing directory to Web ***${NC}';
 cd DealFinder.Web/
 
-echo '${GREEN}*** Updating packages... ***${NC}';
+echo -e '${GREEN}*** Updating packages... ***${NC}';
 yarn
 
-echo '${GREEN}*** Compiling angular files... ***${NC}';
+echo -e '${GREEN}*** Compiling angular files... ***${NC}';
 ng build --prod
 
-echo '${GREEN}*** Changing directory to Api ***${NC}';
+echo -e '${GREEN}*** Changing directory to Api ***${NC}';
 cd ../DealFinder.Api/
 
-echo '${GREEN}*** Compiling dotnet files... ***${NC}';
+echo -e '${GREEN}*** Compiling dotnet files... ***${NC}';
 dotnet restore
 
-echo '${GREEN}*** Starting dotnet process... ***${NC}';
+echo -e '${GREEN}*** Starting dotnet process... ***${NC}';
 dotnet run > stdout.txt 2> stderr.txt &
