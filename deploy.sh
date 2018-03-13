f01e6fed8d4183b325d1a1a5478ce77d3528c18d
Milosz Dura <milosz.md@wp.pl>
2018-03-13 14:32:26 +0000

    improving deploy script logging

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
-echo 'Killing dotnet process...';
+GREEN='\033[0;32m' # Green
+NC='\033[0m' # No Color
+
+echo '${GREEN}*** Killing dotnet process... ***${NC}';
 pkill -HUP dotnet
 
-echo 'Getting latest changes from git...';
+echo '${GREEN}*** Getting latest changes from git... ***${NC}';
 git pull
 
-echo 'Changing directory to Web';
+echo '${GREEN}*** Changing directory to Web ***${NC}';
 cd DealFinder.Web/
 
-echo 'Updating packages...';
+echo '${GREEN}*** Updating packages... ***${NC}';
 yarn
 
-echo 'Compiling angular files...';
+echo '${GREEN}*** Compiling angular files... ***${NC}';
 ng build --prod
 
-echo 'Changing directory to Api';
+echo '${GREEN}*** Changing directory to Api ***${NC}';
 cd ../DealFinder.Api/
 
-echo 'Compiling dotnet files...';
+echo '${GREEN}*** Compiling dotnet files... ***${NC}';
 dotnet restore
 
-echo 'Starting dotnet process...';
+echo '${GREEN}*** Starting dotnet process... ***${NC}';
 dotnet run > stdout.txt 2> stderr.txt &
